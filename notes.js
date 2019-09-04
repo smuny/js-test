030f092b733993b2529d213f02e41fcdd9b01121
github username <sharonymun@gmail.com>
2019-09-04 14:15:42 -0400

    for in loop

--- notes.js
+++ notes.js
@@ -88,3 +88,66 @@ for(let i = 0; i < 10; i++){
     }
     console.log('Number '+ i);
 }
+
+// WHILE LOOP
+
+// let i = 0;
+
+// while(i < 10){
+//     console.log('Number ' + i);
+//     i++;
+// }
+
+// DO WHILE
+
+// let i = 100;
+
+// do{
+//     console.log('Number ' + i);
+//     i++;
+// }
+
+// while(i < 10);
+
+// LOOP THROUGH ARRAY
+
+const cars = ['Ford', 'Chevy', 'Honda', 'Toyota'];
+
+// for(let i = 0; i < cars.length; i++){
+//     console.log(cars[i]);
+// }
+
+// FOR EACH
+// cars.forEach(function(car){
+//     console.log(car)
+// });
+
+// MAP
+// const users = [
+//     {id:1, name:'Sharon'},
+//     {id:2, name:'Janet'},
+//     {id:3, name:'Helen'},
+//     {id:4, name:'Sam'}
+// ];
+
+// const ids = users.map(function(user){
+//     return user.id;
+// });
+
+// const names = users.map(function(user){
+//     return user.name
+// });
+
+// console.log(names);
+
+// FOR IN LOOP
+
+// const user = {
+//     firstName: 'Sharon',
+//     lastName: 'Mun',
+//     age: 27
+// };
+
+// for(let x in user){
+//     console.log(`${x}: ${user[x]}`);
+// };
